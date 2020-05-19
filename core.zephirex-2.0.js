c31ce1e24e4a8e02ca26415ff31995d2eac084ad
teknostatik7 <teknostatik7@users.noreply.github.com>
2020-05-18 20:38:04 -0400

    File uploads

---
+++ core.zephirex-2.0.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+process.env.UV_THREADPOOL_SIZE = 128;
+
+// Initiate variables.
+window = {};
+window.Pairs = [];
+window.balances = [];
+window.balanceSheet = '';
+
+window.config = {
+	senderEmailService: 'gmail',
+	senderEmail: 'sender_email_address',
+	senderEmailPassword: 'sender_email_password',
+	recipientEmail: 'notification_recipient_email_address',
+	passphrase: 'coinbase_pro_API_PASSPHRASE',
+	secret: 'coinbase_pro_API_SECRET',
+	key: 'coinbase_pro_API_KEY',
+};
+
+window.xFactor = .75; 				// Balance usage strength percentage 0 - 1 ; 1 = 100%
+window.minMultiplier = 1.5; 	// Minimum purchase multiplier
+window.fee = 0.0095; 					// Imaginary fee ; higher == low risk ; lower == more frequent trades;
+window.delayMs = 1000;				// Allow 1000 ms between trades to avoid limit
+
+window.pairs = [
+	'BTC-USD',
+	'ETH-USD',
+	'ZRX-BTC',
+	'ETH-BTC',
+	'ZRX-USD',
+	'LTC-USD',
+	'LTC-BTC',
+];
+
+const CoinbasePro = require('coinbase-pro');
+const transporter = require('transporter');
+const director = require('director');
+const publicClient = require('publicClient');
+const websocket = require('websocket');
+
+director();
